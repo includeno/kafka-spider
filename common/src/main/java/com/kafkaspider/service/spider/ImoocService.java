9ebb75d8210b0b683e664ba676d32a2bbf3502de
includeno <includeno@126.com>
2022-03-08 13:35:32 +0800

    0.0.1 change 变更项目结构

---
+++ common/src/main/java/com/kafkaspider/service/spider/ImoocService.java
@@ -0,0 +1,134 @@
+package com.kafkaspider.service.spider;
+
+import com.kafkaspider.config.SeleniumConfig;
+import com.kafkaspider.service.CleanService;
+import com.kafkaspider.service.ContentService;
+import com.kafkaspider.service.MatchService;
+import com.kafkaspider.util.GlobalDateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Pattern;
+
+@Service
+@Slf4j
+public class ImoocService implements MatchService, ContentService, CleanService {
+    public static final String[] patterns = new String[]{
+            "https://www.imooc.com/article/(.+)",//https://www.imooc.com/article/303392
+    };
+
+    @Override
+    public boolean match(String url) {
+        for (String pattern : patterns) {
+            Pattern p = Pattern.compile(pattern);
+            if (p.matcher(url).matches()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public WebDriver getDriver() {
+        WebDriver chrome = SeleniumConfig.getWebDriver(true);
+        return chrome;
+    }
+
+    @Override
+    public void wait(WebDriver chrome, String url) {
+        WebDriverWait wait = new WebDriverWait(chrome, 30, 1);
+        WebElement searchInput = wait.until(new ExpectedCondition<WebElement>() {
+            @Override
+            public WebElement apply(WebDriver text) {
+                return text.findElement(By.className("detail-content"));
+            }
+        });
+        log.info("wait article completed");
+        //class showMore
+        //拉到页面底部
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) chrome;
+        javascriptExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        //class nP21pp 展开阅读全文
+        try {
+            WebElement button = chrome.findElement(By.className("showMore"));
+            if (button != null) {
+                log.warn("检测到showMore按钮");
+                button.click();
+            }
+        } catch (NoSuchElementException e) {
+            //e.printStackTrace();
+            log.warn("Unable to locate element: {\"method\":\"css selector\",\"selector\":\".showMore\"}");
+        }
+    }
+
+    @Override
+    public String getMainContent(WebDriver chrome, String url) {
+        WebElement content = chrome.findElement(By.className("detail-content"));
+        String ans = content.getText();
+        if (ans != null && !ans.equals("")) {
+            log.info("getMainContent completed:" + ans.length());
+            return ans;
+        } else {
+            log.error("getMainContent error " + ans);
+            return "";
+        }
+    }
+
+    @Override
+    public String getTitle(WebDriver chrome, String url) {
+        WebElement content = chrome.findElement(By.className("detail-title"));
+        String ans = content.getText();
+        if (ans != null && !ans.equals("")) {
+            log.info("getTitle completed " + ans);
+            return ans;
+        } else {
+            log.error("getTitle error " + ans);
+            return "";
+        }
+    }
+
+    @Override
+    public String getTag(WebDriver chrome, String url) {
+        String ans = "";
+
+        return ans;
+    }
+
+    @Override
+    public Date getTime(WebDriver chrome, String url) {
+        //class mess 寻找时间
+        List<WebElement> mess = chrome.findElements(By.className("spacer"));
+        Date res = new Date();
+        for(WebElement content:mess){
+            String ans = content.getText();
+            if (ans != null && !ans.equals("")) {
+                res = GlobalDateUtil.convertFull_2(ans);
+                if(res!=null){
+                    break;
+                }
+            }
+        }
+        log.info("getTime completed " + res.toString());
+        return res;
+    }
+
+    @Override
+    public Integer getView(WebDriver chrome, String url) {
+        WebElement content = chrome.findElement(By.className("spacer-2"));
+        String ans = content.getText();
+        Integer view=-1;
+        view=Integer.parseInt(ans);
+        log.info("getView completed " + view);
+        return view;
+    }
+
+    @Override
+    public String cleanUrl(String url) {
+        return null;
+    }
+}
---
+++ common/src/main/java/com/kafkaspider/service/spider/TencentCloudService.java
@@ -0,0 +1,185 @@
+package com.kafkaspider.service.spider;
+
+import com.kafkaspider.config.SeleniumConfig;
+import com.kafkaspider.service.CleanService;
+import com.kafkaspider.service.ContentService;
+import com.kafkaspider.service.MatchService;
+import com.kafkaspider.util.GlobalDateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Pattern;
+
+@Service
+@Slf4j
+public class TencentCloudService implements MatchService, ContentService, CleanService {
+    public static final String[] patterns = new String[]{
+            "https://cloud.tencent.com/developer/article/(.+)",//https://cloud.tencent.com/developer/article/1927707
+    };
+
+    @Override
+    public boolean match(String url) {
+        for (String pattern : patterns) {
+            Pattern p = Pattern.compile(pattern);
+            if (p.matcher(url).matches()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public WebDriver getDriver() {
+        WebDriver chrome = SeleniumConfig.getWebDriver(false);
+        return chrome;
+    }
+
+    @Override
+    public void wait(WebDriver chrome, String url) {
+        WebDriverWait wait = new WebDriverWait(chrome, 30, 1);
+        WebElement searchInput = wait.until(new ExpectedCondition<WebElement>() {
+            @Override
+            public WebElement apply(WebDriver text) {
+                return text.findElement(By.className("J-articlePanel"));
+            }
+        });
+        log.info("wait article completed");
+
+        //拉到页面底部
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) chrome;
+        javascriptExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        //class nP21pp 展开阅读全文
+        try {
+            WebElement button = chrome.findElement(By.className("toggle-link"));
+            if (button != null) {
+                log.warn("检测到阅读原文按钮");
+                button.click();
+            }
+        } catch (NoSuchElementException e) {
+            //e.printStackTrace();
+
+        }
+        finally {
+            return;
+        }
+    }
+
+    @Override
+    public String getMainContent(WebDriver chrome, String url) {
+        //class J-articleContent
+        WebElement content = chrome.findElement(By.className("J-articleContent"));
+        String ans = content.getText();
+        if (ans != null && !ans.equals("")) {
+            log.info("getMainContent completed: length " + ans.length());
+            return ans;
+        } else {
+            log.error("getMainContent error:" + ans);
+            return "";
+        }
+    }
+
+    @Override
+    public String getTitle(WebDriver chrome, String url) {
+        //class J-articleTitle
+        WebElement title = chrome.findElement(By.className("J-articleTitle"));
+        String ans = title.getText();
+        if (ans != null && !ans.equals("")) {
+            log.info("getTitle completed:" + ans);
+            return ans;
+        } else {
+            log.error("getTitle error:" + ans);
+            return "";
+        }
+    }
+
+    @Override
+    public String getTag(WebDriver chrome, String url) {
+        //class col-tag数组
+        String ans = "";
+//        try {
+//            List<WebElement> tags = chrome.findElements(By.className("col-tag"));
+//            if (tags != null && tags.size() > 0) {
+//                StringBuffer stringBuffer = new StringBuffer();
+//                for (WebElement element : tags) {
+//                    stringBuffer.append(element.getText() + " ");
+//                }
+//                ans = stringBuffer.toString();
+//                log.info("getTag completed " + ans);
+//            }
+//        } catch (NoSuchElementException e) {
+//            e.printStackTrace();
+//            log.error("getTag error " + ans);
+//        }
+        return ans;
+    }
+
+    @Override
+    public Date getTime(WebDriver chrome, String url) {
+        //class article-info -> tag time
+        List<WebElement> list = chrome.findElements(By.className("article-info"));
+        Date res = null;
+        for(WebElement element:list){
+            try {
+                WebElement timeElement=element.findElement(By.tagName("time"));
+                if(timeElement!=null){
+
+                    String ans = timeElement.getAttribute("title");
+                    System.out.println("timeElement.getAttribute():"+ans);
+
+                    if (ans != null && !ans.equals("")) {
+                        res = GlobalDateUtil.convert3(ans);
+                    }
+                    log.info("getTime completed:" + res);
+                    return res;
+                }
+            }
+            catch (Exception e){
+                continue;
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public Integer getView(WebDriver chrome, String url) {
+        List<WebElement> list = chrome.findElements(By.className("article-info"));
+        Integer view=-1;
+        for(WebElement element:list){
+            if(element!=null){
+                String ans = element.getText();
+                log.warn("getView:"+ans);
+                if(ans.startsWith("阅读")){
+                    try {
+                        ans=ans.split(" ")[1];
+                        if(ans.endsWith("K")){
+                            ans=ans.split("K")[0];
+                            Double base=Double.parseDouble(ans);
+                            view=(int)(base*1000);
+                            log.info("getView completed " + view);
+                        }
+                        else{
+                            view=Integer.parseInt(ans);
+                            log.info("getView completed " + view);
+                        }
+                        return view;
+                    }
+                    catch (Exception e){
+                        log.info("getView error ");
+                        continue;
+                    }
+                }
+            }
+        }
+        return view;
+    }
+
+    @Override
+    public String cleanUrl(String url) {
+        return null;
+    }
+}
