930309140b97783293adfc6553a3a90ac05fa6e5
includeno <includeno@126.com>
2022-03-08 13:42:36 +0800

    0.0.1 更新controller

--- center/src/main/java/com/kafkaspider/controller/SpiderTaskController.java
+++ center/src/main/java/com/kafkaspider/controller/SpiderTaskController.java
@@ -1,14 +1,19 @@
 package com.kafkaspider.controller;
 
 import com.kafkaspider.request.SubmitRequest;
+import com.kafkaspider.service.SubmitService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpiderTaskController {
 
+    @Autowired
+    SubmitService submitService;
+
     @PostMapping("/task")
     public void submitSpiderTask(SubmitRequest request){
-
+        submitService.submitFunction(request);
     }
 }
