c6a46bed58cec9090caf843310d64b20aaf1d77f
includeno <includeno@126.com>
2022-03-12 16:01:27 +0800

    0.0.1 调整爬虫消费者，修改多线程处理流程

--- common/src/main/java/com/kafkaspider/config/KafkaTopicString.java
+++ common/src/main/java/com/kafkaspider/config/KafkaTopicString.java
@@ -3,9 +3,9 @@ package com.kafkaspider.config;
 public class KafkaTopicString {
 
     //spider
-    public static final String spidertask ="spidertask-2022031102";//spidertask
-    public static final String spidertask_slow ="spidertask-slow-2022031102";//spidertask-slow
-    public static final String spiderresult="spiderresult-2022031102";//spiderresult
+    public static final String spidertask ="spidertask-20220312";//spidertask
+    public static final String spidertask_slow ="spidertask-slow-20220312";//spidertask-slow
+    public static final String spiderresult="spiderresult-20220312";//spiderresult
 
     //Spark监听
 
